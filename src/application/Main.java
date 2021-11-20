3aaccce7808804074539cf6951b6d9a3da80e8e3
Peggy <peggy22509385@gmail.com>
2021-11-20 14:16:24 +0100

    create project

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import controller.StartViewController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.StartView;
+
+public class Main extends Application {
+	private static final int SCENE_WIDTH = 800, SCENE_HEIGHT = 500;
+	private static final String CSS_PATH = "application.css";
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		StartViewController startViewController = new StartViewController(new StartView());
+		Scene scene = new Scene(startViewController.getStartView(), SCENE_WIDTH, SCENE_HEIGHT);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Final Project");
+		primaryStage.show();
+	}
+}
